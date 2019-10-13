17fd5ae5ce145e4b9088a717333ba64bcddb26e9
lfish <shineccy@outlook.com>
2019-10-13 22:50:12 +0800

    添加项目文件。

---
+++ rawFood/rawFood.cpp
@@ -0,0 +1,16 @@
+﻿// rawFood.cpp : 定义应用程序的入口点。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "rawFood.h"
+
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	MessageBoxA(NULL, "hello", "introduct", MB_OK);
+	return 0;
+}
